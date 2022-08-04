706fdff73e906d4b462d6ce73e9681a4226228f6
Clarkinr <robclarkin1@gmail.com>
2022-08-04 14:46:04 +0000

    updates to index.html

--- twos/models.py
+++ twos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0,"Pending"),(1,"Approved"))
+STATUS = ((0, "Pending"), (1, "Approved"))
 
 TIME_SLOTS = (
     ('18:00', '18:00'),
@@ -22,7 +22,7 @@ NUM_PEOPLE = (
 
 
 class booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "user_book")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_book")
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email_address = models.EmailField(null=True, blank=True)
@@ -31,17 +31,23 @@ class booking(models.Model):
     reservation_made = models.DateTimeField(auto_now=True)
     Status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-reservation_made"]
+
     def __str__(self):
         return self.first_name
 
 
 class feedback(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "user_feedback")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_feedback")
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     comment = models.TextField(max_length=350)
     feedback_made = models.DateTimeField(auto_now=True)
     Status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-feedback_made"]
 
     def __str__(self):
         return self.first_name
@@ -50,6 +56,11 @@ class feedback(models.Model):
 class menu(models.Model):
     menu_image = CloudinaryField('image', default='placeholder')
     menu_time = models.TextField()
+    menu_made = models.DateTimeField(auto_now=True)
+    Status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-menu_made"]
 
     def __str__(self):
         return self.menu_time
--- twos/views.py
+++ twos/views.py
@@ -3,19 +3,21 @@ from django.views import generic
 from .models import booking, menu, feedback
 
 
-class bookingList(generic.ListView):
+class BookingList(generic.ListView):
     model = booking
-    queryset = booking.objects.filter(Status=1)
+    queryset = booking.objects.filter(Status=1).order_by("-reservation_made")
     template_name = 'bookings.html'
 
 
-class menuList(generic.ListView):
+class MenuList(generic.ListView):
     model = menu
+    queryset = menu.objects.filter(Status=1).order_by("-menu_made")
     template_name = 'index.html'
     paginate_by = 2
 
 
-class feedbackList(generic.ListView):
+class FeedbackList(generic.ListView):
     model = feedback
+    queryset = feedback.objects.filter(Status=1).order_by("-feedback_made")
     template_name = 'index.html'
-    paginate_by = 4
+    paginate_by = 3
